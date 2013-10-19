7a4982822aef49a6a8d610db05954d7c0e3f54ee
loremarsy <lore_marsy_blackeyes@yahoo.com>
2013-10-20 01:12:48 +0300

    data models

--- server/crossco/models/models.py
+++ server/crossco/models/models.py
@@ -14,7 +14,26 @@ class User(db.Model, UserMixin):
 #user1 = User(username=u'test', password=u'test')
 #db.session.add(user1)
 #db.session.commit()
-
+class Action(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer)
+	fb_id = db.Column(db.String(40))
+	service_id = db.Column(db.Integer)
+	action = db.Column(db.Unicode(64))
+	date_time = db.Column(db.DateTime)
+	message =db.Column(db.Unicode(200))
+	
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(64))
+    parent_id =db.Column(db.Integer)
+	
+class Services(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cat_id = db.Column(db.Integer)	
+	fb_serv_id = db.Column(db.String(40))
+	
+	
 
 class Connection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
